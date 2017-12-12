c2e6a43fa96bd3ae31e591f3ea743361da03a09d
Matan Kushner <matchai@me.com>
2017-12-11 22:57:29 -0500

    Clean up anime command

--- src/media/index.js
+++ src/media/index.js
@@ -2,6 +2,18 @@ const api = require('../api');
 const query = require('./query');
 const toMarkdown = require('to-markdown');
 
+const anilistLogo = "https://anilist.co/img/logo_al.png";
+const capitalize = str => str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
+
+const shorten = str => {
+    const markdown = toMarkdown(str);
+    if (markdown.length > 400) {
+        return markdown.substring(0, 400) + '...'
+    } else {
+        return markdown;
+    }
+}
+
 const search = async (searchArg, type) => {
     const response = await api(query, {
         search: searchArg,
@@ -16,18 +28,22 @@ const search = async (searchArg, type) => {
 }
 
 const toDiscordObject = (media) => {
-    const score = media.averageScore != null ? `Score: ${media.averageScore}% ` : '';
-    const status = media.averageScore != null ? `Status: ${media.status.toLowerCase()} ` : '';
+    const score = media.averageScore != null ? `Score: ${media.averageScore}%` : '';
+    const status = media.averageScore != null ? `Status: ${capitalize(media.status)}` : '';
 
     return {
-        title: media.title.romaji,
-        url: media.siteUrl,
+        author: {
+            name: media.title.romaji,
+            url: media.siteUrl,
+            icon_url: anilistLogo
+        },
         thumbnail: {
             url: media.coverImage.large,
         },
-        description: toMarkdown(media.description).substring(0, 400) + '...',
+        description: shorten(media.description),
         footer: {
-            text: score + status
+            // Use the en quad space after score to not get stripped by Discord
+            text: `${score}  ${status}`
         }
     }
 }
